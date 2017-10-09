f4da5e8d9236fd14d3fae462fae9c3c4679f944f
arvikachristy <anastasia.arvika@gmail.com>
2017-10-09 22:18:06 +0100

    display the JSON :baby_chick:

--- dataIngest/ingest/app.py
+++ dataIngest/ingest/app.py
@@ -3,8 +3,10 @@
 Here, you would set routes for your data ingest application. Routes should define the functionality. In this example, we have ways of getting data manually, and getting and updating a schedule. This schedule might be used at some later point to get data automatically (i.e. without a user having to specifically request it).
 
 """
+import urllib.request
+import json
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from . import db
 from . import kiva
@@ -12,28 +14,25 @@ from . import schedule
 from . import schema
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello, world!!!'
-
+    response = urllib.request.urlopen('https://api.kivaws.org/v1/loans/newest.json')
+    res = response.read()
+    data = json.loads(res)
+    return jsonify(data)
 
 @app.before_first_request
 def setup_database():
     db.setup()
-
 
 @app.route('/data', methods=['GET'])
 def get_data():
     data = kiva.get()
     transformedData = schema.transform(data)
     db.insert(transformedData)
-    return ('', 204)
-
-@app.route('/kiva', methods=['GET'])
-    def printMe():
-        return kiva.get()  
+    return data
 
 @app.route('/schedule', methods=['GET', 'POST'])
 def get_or_update_schedule():
@@ -42,4 +41,3 @@ def get_or_update_schedule():
 
     if request.method == 'POST':
         return schedule.update()
-
