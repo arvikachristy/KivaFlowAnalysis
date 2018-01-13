357edaa3cd3ed6d393f0bc4d960ca53775bd4d1b
arvikachristy <anastasia.arvika@gmail.com>
2018-01-13 13:58:48 +0000

    topological query :worried:

---
+++ topological-analysis/topological_query.sql
@@ -0,0 +1,23 @@
+#######bonferroni Query
+
+#1 Add Nl and Nr data to the table
+create table test_data_gb_au as SELECT 
+* FROM test_data_gbau S
+INNER JOIN (
+    SELECT 
+    country_from as dupli1,
+    sum(cast(flow as int)) as n_l
+    from test_data_gbau
+    GROUP BY country_from
+) C ON S.country_from = dupli1
+INNER JOIN (
+    SELECT 
+    country_to as dupli2,
+    sum(cast(flow as int)) as n_r
+    from test_data_gbau
+    GROUP BY country_to
+) X ON S.country_to = dupli2
+ORDER BY country_from, country_to
+
+#2 delete dupli from console 
+#3 
